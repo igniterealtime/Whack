f5650fa0e595319424faa522c7ab0e4b4f3647aa
Gaston Dombiak <gato@jivesoftware.com>
2006-11-17 17:30:54 +0000

    Removed duplicated code.

--- sample/weather/source/java/org/jivesoftware/weather/ExternalWeatherComponent.java
+++ sample/weather/source/java/org/jivesoftware/weather/ExternalWeatherComponent.java
@@ -1,12 +1,7 @@
 package org.jivesoftware.weather;
 
 import org.jivesoftware.whack.ExternalComponentManager;
-import org.xmpp.component.Component;
-import org.xmpp.component.ComponentManager;
 import org.xmpp.component.ComponentException;
-import org.xmpp.component.ComponentManagerFactory;
-import org.xmpp.packet.Packet;
-import org.xmpp.packet.JID;
 
 /**
  * This is an example of how to make a component run as an external component. This examples
@@ -21,7 +16,6 @@ public class ExternalWeatherComponent {
         // Create a manager for the external components that will connect to the server "localhost"
         // at the port 5222
         final ExternalComponentManager manager = new ExternalComponentManager("localhost", 5222);
-        ComponentManagerFactory.setComponentManager(manager);
         // Set the secret key for this component. The server must be using the same secret key
         // otherwise the component won't be able to authenticate with the server. Check that the
         // server has the properfy "component.external.secretKey" defined and that it is using the
