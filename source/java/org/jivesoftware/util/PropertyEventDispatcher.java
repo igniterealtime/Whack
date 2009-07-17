a58924657f0f20a45c35dc67f8057f10b75eda04
Gaston Dombiak <gato@jivesoftware.com>
2009-07-17 18:15:46 +0000

    Updated license to Apache

--- source/java/org/jivesoftware/util/PropertyEventDispatcher.java
+++ source/java/org/jivesoftware/util/PropertyEventDispatcher.java
@@ -1,12 +1,17 @@
 /**
- * $RCSfile$
- * $Revision: 1705 $
- * $Date: 2005-07-26 10:10:33 -0700 (Tue, 26 Jul 2005) $
+ * Copyright 2004-2009 Jive Software.
  *
- * Copyright (C) 2004-2005 Jive Software. All rights reserved.
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This software is published under the terms of the GNU Public License (GPL),
- * a copy of which is included in this distribution.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.jivesoftware.util;
